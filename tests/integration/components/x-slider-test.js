ba88d7510815c0fa3ccbc03ba59a05cf8b9cfc79
Robdel12 <robertdeluca19@gmail.com>
2016-01-22 14:35:55 -0600

    Updating x-slider to the latest and greatest

---
+++ tests/integration/components/x-slider-test.js
@@ -0,0 +1,69 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describeComponent, it } from 'ember-mocha';
+import { describe, beforeEach } from 'mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describeComponent('x-slider', 'Integration: XSliderComponent', { integration: true }, function() {
+  it('renders', function() {
+    this.render(hbs`{{x-slider min=0 max=100 step=1 value=number}}`);
+    expect(this.$()).to.have.length(1);
+  });
+
+  describe("setting an intial vaule", function () {
+    beforeEach(function() {
+      this.set('number', 5);
+      this.render(hbs`{{x-slider min=0 max=100 step=1 value=number}}`);
+    });
+
+    it("sets the intial value", function() {
+      expect(this.$('.x-slider').val()).to.equal('5');
+    });
+
+    describe("setting the dom value", function () {
+      beforeEach(function() {
+        this.set('number', 10);
+        this.render(hbs`{{x-slider min=0 max=100 step=1 value=number}}`);
+      });
+
+      it("updates the value", function() {
+        expect(this.$('.x-slider').val()).to.equal('10');
+      });
+    });
+  });
+
+  describe("setting bound attributes", function () {
+    beforeEach(function() {
+      this.setProperties({
+        min: "5",
+        max: "20",
+        step: "5",
+        value: "10",
+        name: "my-slider",
+        list: "my-list"
+      });
+      this.render(hbs`{{x-slider min=min max=max step=step name=name value=value list=list}}`);
+    });
+
+    it("binds the min attribute properly", function() {
+      expect(this.$('.x-slider').prop('min')).to.equal('5');
+    });
+
+    it("binds the max attribute properly", function() {
+      expect(this.$('.x-slider').prop('max')).to.equal('20');
+    });
+
+    it("binds the value attribute properly", function() {
+      expect(this.$('.x-slider').prop('value')).to.equal('10');
+    });
+
+    it("binds the name attribute properly", function() {
+      expect(this.$('.x-slider').prop('name')).to.equal('my-slider');
+    });
+
+    it("binds the list attribute properly", function() {
+      expect(this.$('.x-slider').attr('list')).to.equal('my-list');
+    });
+
+  });
+});
